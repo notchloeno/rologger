8c8aaa1b65e4b737e2837ea5537f186ca96a00bd
Josh <joshua.roberts@gamefam.com>
2023-03-27 14:46:54 +0100

    Update LoggerConfig.lua

--- LoggerConfig.lua
+++ LoggerConfig.lua
@@ -7,13 +7,12 @@ return {
         Error = 4,
         Critical = 5
     },
-    -- These end with a space for formatting reasons
     LevelText = {
-        "[Debug] ",
-        "[Info] ",
-        "[Warn] ",
-        "[ERROR] ",
-        "[CRITICAL!] "
+        "[Debug]",
+        "[Info]",
+        "[Warn]",
+        "[ERROR]",
+        "[CRITICAL!]"
     },
 
     -- Default local options
@@ -21,5 +20,6 @@ return {
         Level = 1,  -- Messages with level strictly below this are ignored
         Depth = 1000,  -- How many messages will be stored in memory
         TracebackLevel = 4,  -- Messages at or above this level will generate a traceback
+        IncludeTimestamp = false,  -- Add a timestamp to message
     }
 }
